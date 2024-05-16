204fb5a491e8f43499142fae5aaf8b7c8065483d
Jassocc <clnearle@gmail.com>
2024-05-16 06:24:01 +0000

    task -

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -63,11 +63,11 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
     name_db = os.getenv("PERSONAL_DATA_DB_NAME", "")
-    connection = mysql.connector.connect(user=username,
-                                         password=password,
-                                         host=host,
-                                         port=3306,
-                                         database=name_db)
+    connection = mysql.connector.connecttion.MySQLConnection(user=username,
+                                                             password=password,
+                                                             host=host,
+                                                             port=3306,
+                                                             database=name_db)
     return connection
 
 
