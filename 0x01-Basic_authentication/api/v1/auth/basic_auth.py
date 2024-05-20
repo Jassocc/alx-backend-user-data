3dfe9ebdad2cc5c2343503b35b6a9f68c98ac667
Jassocc <clnearle@gmail.com>
2024-05-20 11:56:05 +0000

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,9 @@
 script for task 7
 """
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -21,3 +24,21 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """
+        returns decoded base 64
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            benc = base64_authorization_header.encode('utf-8')
+            bdec = b64decode(benc)
+            dec_val = bdec.decode('utf-8')
+            return dec_val
+        except Exception:
+            return None
