57fd12495efb9ebec4ba2eb4af8c4d9b3922ae33
Jassocc <clnearle@gmail.com>
2024-05-20 11:38:31 +0000

    task 6

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -35,6 +35,10 @@ class Auth:
         """
         if request is None:
             return None
+        if not request.headers.get("Authorization"):
+            return None
+        else:
+            return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+script for task 7
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    basic auth class
+    """
+    pass
