1f6e04774db72babf523c19f1264ac2e1946220f
Jassocc <clnearle@gmail.com>
2024-05-22 18:37:50 +0000

    update

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -3,30 +3,33 @@
 handles routes for task 7
 """
 from flask import request, jsonify, abort
+from typing import Tuple
 from models.user import User
 import os
 from api.v1.views import app_views
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def session_login() -> str:
+def session_login() -> Tuple[str, int]:
+    nowt = {"error": "no user found for this email"}
     user_email = request.form.get('email')
-    if not user_email:
+    if user_email is None or len(user_email.strip()) == 0:
         return jsonify({"error": "email missing"}), 400
     user_password = request.form.get('password')
-    if not user_password:
+    if user_password is None or len(password.strip()) == 0:
         return jsonify({"error": "password missing"}), 400
-    user = User.search({'email': user_email})
-    if not user:
-        return jsonify({"error": "no user found for this email"}), 404
-
-    for us in user:
-        if us.is_valid_password(user_password):
-            from api.v1.app import auth
-            session_id = auth.create_session(us.id)
-            user_json = jsonify(us.to_json())
-            user_json.set_cookie(os.getenv('SESSION_NAME'), session_id)
-            return user_json
+    try:
+        user = User.search({'email': user_email})
+    except Exception:
+        return jsonify(nowt), 404
+    if len(user) <= 0:
+        return jsonify(nowt), 404
+    if user[0].is_valid_password(user_password):
+        from api.v1.app import auth
+        session_id = auth.create_session(getattr(user[0], 'id'))
+        us = jsonify(user[0].to_json())
+        us.set_cookie(os.getenv('SESSION_NAME'), session_id)
+        return us
 
     return jsonify({"error": "wrong password"}), 401
 
