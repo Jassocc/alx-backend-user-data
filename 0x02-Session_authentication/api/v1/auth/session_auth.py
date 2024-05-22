c28ed4d9a411bc00563d2300e7006db660da785d
Jassocc <clnearle@gmail.com>
2024-05-22 08:35:49 +0000

    task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+script for task 2
+"""
+from api.v1.auth.auth import Auth
+import uuid
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """
+    defines auth's for sessions
+    """
+    pass
