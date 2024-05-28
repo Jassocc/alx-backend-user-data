20aeed2b50c6096d87870c42427fe611393918a0
Jassocc <clnearle@gmail.com>
2024-05-28 11:49:20 +0000

    task 6

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+flask app for auth
+"""
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome():
+    """
+    returns json payload
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
