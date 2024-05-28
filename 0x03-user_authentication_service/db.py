bc067ae5e1e987b09f8fbab6f5975138cd6a136c
Jassocc <clnearle@gmail.com>
2024-05-28 09:35:06 +0000

    task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -45,9 +45,9 @@ class DB:
         """
         funds a user
         """
-        if not kwargs:
+        new_user = self._session.query(User).filter_by(**kwargs).first()
+        if kwargs is None:
             raise InvalidRequestError
-        try:
-            return self._session.query(User).filter_by(**kwargs).first()
-        except NoResultFound:
-            return None
+        if new_user is None:
+            raise NoResultFound
+        return new_user
