ad23bc9bb032420a3669172e8e5dbe03eca9d10b
Jassocc <clnearle@gmail.com>
2024-05-28 19:40:54 +0000

    task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -51,7 +51,8 @@ class Auth:
         except NoResultFound:
             return False
         else:
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
 
     def create_session(self, email: str) -> str:
         """
