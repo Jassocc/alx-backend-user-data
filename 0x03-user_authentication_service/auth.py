800ef4e7a540639fcdbb3f6391f8c4a6eb9ae23b
Jassocc <clnearle@gmail.com>
2024-05-29 08:09:57 +0000

    task 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -99,6 +99,17 @@ class Auth:
         self._db.update_user(exis_user.id, reset_token=t)
         return t
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        resets password
+        """
+        try:
+            usr = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        pwd = _hash_password(password)
+        self._db.update_user(usr.id, hashed_password=pwd, reset_token=None)
+
 
 def _generate_uuid() -> str:
     """
