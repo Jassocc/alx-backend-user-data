09fcee015b13e838eceab8942add8f9a720381cb
Jassocc <clnearle@gmail.com>
2024-05-29 09:26:52 +0000

    task 20

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,16 +1,90 @@
 #!/usr/bin/env python3
 """
-Main file
+trsting for flask
 """
+import requests
 
-from db import DB
-from user import User
 
-my_db = DB()
+URL = 'http://localhost:5000'
 
-user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-print(user_1.id)
 
-user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
+def register_user(email: str, password: str) -> None:
+    """
+    test reg user
+    """
+    response = {"email": email, "password": password}
+    result = requests.post(f'{URL}/users', response)
+    assert result.status_code == 201
 
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """
+    login with wrong password
+    """
+    response = {"email": email, "password": password}
+    result = requests.post(f'{URL}/sessions', response)
+    assert result.status_code == 401
+
+def log_in(email: str, password: str) -> str:
+    """
+    log in user
+    """
+    response = {"email": email, "password": password}
+    result = requests.post(f'{URL}/sessions', response)
+    assert result.status_code == 200
+
+def profile_unlogged() -> None:
+    """
+    profile unlog
+    """
+    ses = {"session_id": ""}
+    result = requests.get(f'{URL}/profile', ses)
+    assert result.status_code == 403
+
+def profile_logged(session_id: str) -> None:
+    """
+    profile in log
+    """
+    ses = {"session_id": session_id}
+    result = requests.get(f'{URL}/profile', ses)
+    assert result.status_code == 200
+
+def log_out(session_id: str) -> None:
+    """
+    log user ouit
+    """
+    ses = {"session_id": session_id}
+    result = requests.delete(f'{URL}/sessions', ses)
+    assert result.status_code == 204
+
+def reset_password_token(email: str) -> str:
+    """
+    pwd reset
+    """
+    eml = {"email": email}
+    result = requests.post(f'{URL}/reset_password', eml)
+    assert result.status_code == 200
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    eml = {"email": email, "reset_token": reset_token,
+           "new_password": new_password}
+    result = requests.put(f'{URL}/reset_password', eml)
+    assert result.status_code == 200
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
